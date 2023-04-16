de807330e82e7829665a9f34141f04aa0fa3c8d9
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-04-16 18:59:36 -0400

    coded the repos

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -2,9 +2,16 @@ package com.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TechNewsJavaApiApplication {
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to Tech News! The Java version. ";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechNewsJavaApiApplication.class, args);
